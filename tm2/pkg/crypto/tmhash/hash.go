cdaad271248825ecaa135478d2b2f4949512882e
Milosevic02 <milosevicdragan002@gmail.com>
2025-05-21 13:03:56 +0200

    feat(tmhash): introduce sync.Pool for hash reuse and memory optimization

--- tm2/pkg/crypto/tmhash/hash.go
+++ tm2/pkg/crypto/tmhash/hash.go
@@ -3,6 +3,7 @@ package tmhash
 import (
 	"crypto/sha256"
 	"hash"
+	"sync"
 )
 
 const (
@@ -10,15 +11,36 @@ const (
 	BlockSize = sha256.BlockSize
 )
 
-// New returns a new hash.Hash.
+// HashPool provides a pool of hash.Hash instances to reduce memory allocations
+var hashPool = sync.Pool{
+	New: func() interface{} {
+		return sha256.New()
+	},
+}
+
+// New returns a new hash.Hash from the pool.
 func New() hash.Hash {
-	return sha256.New()
+	return hashPool.Get().(hash.Hash)
+}
+
+// ReturnHash returns a hash.Hash to the pool.
+func ReturnHash(h hash.Hash) {
+	h.Reset()
+	hashPool.Put(h)
 }
 
 // Sum returns the SHA256 of the bz.
 func Sum(bz []byte) []byte {
-	h := sha256.Sum256(bz)
-	return h[:]
+	h := hashPool.Get().(hash.Hash)
+	defer func() {
+		h.Reset()
+		hashPool.Put(h)
+	}()
+
+	h.Write(bz)
+	result := make([]byte, Size)
+	h.Sum(result[:0])
+	return result
 }
 
 //-------------------------------------------------------------
@@ -52,15 +74,36 @@ func (h sha256trunc) BlockSize() int {
 	return h.sha256.BlockSize()
 }
 
-// NewTruncated returns a new hash.Hash.
+// truncatedHashPool provides a pool of truncated hash instances
+var truncatedHashPool = sync.Pool{
+	New: func() interface{} {
+		return sha256trunc{
+			sha256: sha256.New(),
+		}
+	},
+}
+
+// NewTruncated returns a new hash.Hash from the truncated pool.
 func NewTruncated() hash.Hash {
-	return sha256trunc{
-		sha256: sha256.New(),
-	}
+	return truncatedHashPool.Get().(hash.Hash)
+}
+
+// ReturnTruncatedHash returns a truncated hash to the pool.
+func ReturnTruncatedHash(h hash.Hash) {
+	h.Reset()
+	truncatedHashPool.Put(h)
 }
 
 // SumTruncated returns the first 20 bytes of SHA256 of the bz.
 func SumTruncated(bz []byte) []byte {
-	hash := sha256.Sum256(bz)
-	return hash[:TruncatedSize]
+	h := hashPool.Get().(hash.Hash)
+	defer func() {
+		h.Reset()
+		hashPool.Put(h)
+	}()
+
+	h.Write(bz)
+	result := make([]byte, sha256.Size)
+	h.Sum(result[:0])
+	return result[:TruncatedSize]
 }
